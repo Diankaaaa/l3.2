40ec3797eb0a6523154563e5c9531d8e92ada8d3
diana <diana@DESKTOP-AE9RS0A>
2020-11-11 17:18:53 +0300

    Добавьте файлы проекта.

---
+++ l3.2/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace l3._2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            try {
+
+                //Получение значения m от пользователя
+                double m = double.Parse(Input.Text);
+                double result = 0;
+
+                for (int i = 100; i <= 1000; i++)
+                {
+                    if ((i * i) % 5 == 0) //Проверка квадрата сотен на кратность 5
+                    {
+                        result += (i * i) / m;
+                    }
+                }
+
+                //Вывод результата 
+                Output.Text = result.ToString();
+            } 
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
